415ac19d86b0ba5d62c12428abee4012a710322a
Oluwaseyi69 <oluwaseyitemitope69@gmail.com>
2024-04-05 11:28:58 +0100

    newly-updated

---
+++ src/main/java/com/example/dockerdev/DockerDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dockerdev/DockerDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
